f83503ffded76a6f007e2e77783c8fcaad516fcd
zhitutransport <zhitu_tu_info@zhitutransport.cn>
2018-09-05 13:48:09 +0800

    城市模拟

---
+++ src/main/java/com/zhitu/simulation_road/SimulationRoadApplication.java
@@ -0,0 +1,12 @@
+package com.zhitu.simulation_road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationRoadApplication.class, args);
+	}
+}
